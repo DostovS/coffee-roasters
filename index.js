f51877158bd35f97b5d1943d958cac05b3fb25f9
DostovS <sohibjondostov04@gmail.com>
2022-08-13 16:55:04 +0500

    work is end

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const sr = ScrollReveal({
 sr.reveal(`.hero-content, .type:nth-child(2), .quality-content, .works-column:nth-child(2)`, {
   interval: 200
 });
+
 const leftReveal = ScrollReveal({
   origin: 'left',
   distance: '20px',
@@ -16,6 +17,7 @@ const leftReveal = ScrollReveal({
 leftReveal.reveal(`.left, .type:nth-child(1), .works-column:last-child`, {
   interval: 200
 });
+
 const rightReveal = ScrollReveal({
   origin: 'right',
   distance: '20px',
@@ -28,38 +30,27 @@ rightReveal.reveal(`.right, .type:nth-child(3), .works-column:first-child`, {
 const hamburger = document.querySelector(".hamburger");
 const aside = document.querySelector('aside');
 const asideTheme = document.querySelector('aside .bx-moon');
-hamburger.onclick = function() {
+hamburger.onclick = function () {
   aside.classList.toggle('asideOpen')
 }
-asideTheme.onclick = function() {
+asideTheme.onclick = function () {
   asideTheme.classList.toggle('bx-moon');
   asideTheme.classList.toggle('bxs-sun');
   body.classList.toggle('active')
 }
 const darkIcon = document.querySelector('.bx-moon');
 const body = document.querySelector('body');
-darkIcon.onclick = function() {
+darkIcon.onclick = function () {
   darkIcon.classList.toggle('bx-moon');
   darkIcon.classList.toggle('bxs-sun');
   body.classList.toggle('active')
 }
 const closeSidebar = document.querySelector('.bxs-home-alt-2');
-closeSidebar.onclick = function() {
+closeSidebar.onclick = function () {
   aside.classList.remove('asideOpen')
   aside.classList.toggle('close');
 }
 
-function scrollHeader() {
-  const nav = document.querySelector('header');
-
-  if(this.scrollY > 200) {
-    nav.classList.add('scroll-header')
-  } else {
-    nav.classList.remove('scroll-header');
-  }
-}
-window.addEventListener('scroll', scrollHeader);
-window.onscroll = function() {myFunction()};
 
 function myFunction() {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -67,3 +58,13 @@ function myFunction() {
   let scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  if (this.scrollY > 560) {
+    scrollTop.classList.add('show-scroll');
+  } else {
+    scrollTop.classList.remove('show-scroll');
+  }
+}
+window.addEventListener('scroll', scrollTop);
